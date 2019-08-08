48a56879c986355b575e43a5852b810095a62fdf
yubachiri <k.i.moso9532@gmail.com>
2019-08-08 10:46:55 +0900

    brushup

--- calculator.rb
+++ calculator.rb
@@ -1,4 +1,4 @@
 puts "input waiting..."
 input = gets # 標準入力を受け付ける
-
-puts input.chomp == 'answer' # inputには改行文字が含まれているためchompしています
+result = input.chomp == 'answer' # inputには改行文字が含まれているためchompしています
+puts result
